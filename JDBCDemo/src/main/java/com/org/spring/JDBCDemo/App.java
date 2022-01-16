06c45176ce39278054967f577d0a21df1ecfd50e
Ashu <raghavrajput297@gmail.com>
2022-01-16 23:32:48 +0530

    file uploaded

---
+++ JDBCDemo/src/main/java/com/org/spring/JDBCDemo/App.java
@@ -0,0 +1,46 @@
+package com.org.spring.JDBCDemo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws SQLException {
+		System.out.println("Hello World!");
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriver(new com.mysql.cj.jdbc.Driver());
+		dataSource.setUrl("jdbc:mysql://localhost:3306/genpact");
+		dataSource.setUsername("root");
+		dataSource.setPassword("Ashish@7386");
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+		String sqlInsert = "INSERT INTO Employee (id, name, salary)" + " VALUES (?, ?, ?)";
+		jdbcTemplate.update(sqlInsert, 1, "Tom", 12345);
+
+		String sqlUpdate = "UPDATE Employee set name=? where id=?";
+		jdbcTemplate.update(sqlUpdate, "tomee@mail.com", 1);
+
+		String sqlSelect = "SELECT * FROM employee";
+		List<Employee> listContact = jdbcTemplate.query(sqlSelect, new RowMapper<Employee>() {
+
+			public Employee mapRow(ResultSet result, int rowNum) throws SQLException {
+				Employee contact = new Employee();
+				contact.setName(result.getString("name"));
+				contact.setId(result.getInt("id"));
+				return contact;
+			}
+		});
+		for (Employee e : listContact) {
+			System.out.println(e);
+		}
+
+	}
+}
